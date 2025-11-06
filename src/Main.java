52b9e8c42171ca495aaff5e16560cee2593a4948
Vetriselvan Karunanithi <166475004+vetrikarunanithi@users.noreply.github.com>
2025-11-06 20:12:37 +0530

    Change server port to use environment variable
    Updated server port configuration to use environment variable.

--- src/Main.java
+++ src/Main.java
@@ -39,7 +39,9 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-            HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+            int port = Integer.parseInt(System.getenv().getOrDefault("PORT", "8080"));
+            HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+
 
             System.out.println("=================================");
             System.out.println("  To-Do List Server Started");
